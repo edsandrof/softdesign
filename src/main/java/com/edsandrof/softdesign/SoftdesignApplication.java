e24d22f67bee50b90e9f0689d0132582ddb81d1e
edsandrof <edsandrofreitas@gmail.com>
2020-09-15 00:06:59 -0300

    first commit

---
+++ src/main/java/com/edsandrof/softdesign/SoftdesignApplication.java
@@ -0,0 +1,13 @@
+package com.edsandrof.softdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftdesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftdesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edsandrof/softdesign/SoftdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsandrof.softdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
