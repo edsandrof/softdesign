79f4fe2ebbfe43466ee07c238572eb1655b58fba
edsandrof <edsandrofreitas@gmail.com>
2020-09-22 21:57:54 -0300

    fixed user 404 not found in cpf check

--- src/main/java/com/edsandrof/softdesign/utils/Utils.java
+++ src/main/java/com/edsandrof/softdesign/utils/Utils.java
@@ -2,20 +2,26 @@ package com.edsandrof.softdesign.utils;
 
 import com.edsandrof.softdesign.dto.MemberVotingStatusDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
 
 public class Utils {
     public static boolean canMemberVote(String cpf) {
-        final String uri = "https://user-info.herokuapp.com/users/";
+        try {
+            final String uri = "https://user-info.herokuapp.com/users/";
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<MemberVotingStatusDTO> response = restTemplate.getForEntity(uri + cpf, MemberVotingStatusDTO.class);
+            RestTemplate restTemplate = new RestTemplate();
+            cpf = "00055562012";
+            ResponseEntity<MemberVotingStatusDTO> response = restTemplate.getForEntity(uri + cpf, MemberVotingStatusDTO.class);
 
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return Objects.requireNonNull(response.getBody()).getStatus().equals(MemberVotingStatusDTO.Status.ABLE_TO_VOTE);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return Objects.requireNonNull(response.getBody()).getStatus().equals(MemberVotingStatusDTO.Status.ABLE_TO_VOTE);
+            }
+            return false;
+        } catch (HttpClientErrorException e) {
+            return false;
         }
-        return false;
     }
 }
